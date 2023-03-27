98950f7c53209a33e7fcfa0aec65e0d174fb5f55
tao20000317 <122748127+tao20000317@users.noreply.github.com>
2023-03-27 03:15:45 +0000

    test

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css"
 export default function About() {
     return (
         <section className={styles.section}>
-            <h1 className={styles.title}>About Bobby Connolly</h1>
+            <h1 className={styles.title}>About Tao</h1>
             <p className={styles.text}>Test</p>
         </section>
     )
